c45b9802e1c98da7740d8fbad307f788424c573f
thrasht <eduardo.mhdz@gmail.com>
2018-02-01 16:56:19 -0600

    Se agrega Edit y New a Movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -32,12 +32,7 @@ namespace Vidly.Controllers
             //return HttpNotFoundResult();
             return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id = " + id);
-        }
-
+        
         public ActionResult IndexAlt(int? pageIndex, string sortBy)
         {
             if (!pageIndex.HasValue)
@@ -96,6 +91,53 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Save(Movie movie)
+        {
+            if(movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
+
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.Single(c => c.Id == id);
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+        
     }
 
 }
